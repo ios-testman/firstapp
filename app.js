c5a52c6f706a35378655a4b21fdd1f9bd2a925a5
okumura <Hirotoshi.Okumura@jrits.ricoh.co.jp>
2013-08-29 00:40:45 +0900

    expressのサンプル実施。 http://gihyo.jp/dev/serial/01/nodejs/0003　3回　Express.jsを使ったWebアプリケーションを構築 express ver.3では、-e をつけなければ、ejsにならない。

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
+var routes = require('./routes/main');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
@@ -14,7 +14,7 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -29,6 +29,8 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/form', routes.form);
+app.post('/create', routes.create);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
